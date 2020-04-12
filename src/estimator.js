98b1246974236ae39e360943aa0a8316ff0dadcc
jeffngugi <ngugimwathi254@gmail.com>
2020-04-12 10:59:44 +0300

    Challenge one for estimation

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,11 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const currentlyInfected = data.reportedCases * 10;
+  return {
+    data: input,
+    impact: {},
+    severeImpact: {}
+  };
+};
 
 export default covid19ImpactEstimator;
