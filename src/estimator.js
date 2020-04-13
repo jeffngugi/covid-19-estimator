85c1be49c9a369f8a9595994ee64a5cf17df04cf
jeffngugi <ngugimwathi254@gmail.com>
2020-04-14 01:38:06 +0300

    Challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -14,11 +14,22 @@ const covid19ImpactEstimator = (data) => {
   //challenge one
   severeImpact.currentlyInfected = reportedCases * 50;
   impact.currentlyInfected = reportedCases * 10;
-  infectionsByRequestedTime = currentlyInfected * 1024;
 
   impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
 
+  //Challenge two
+  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+  severeImpact.severeCasesByRequestedTime =
+    severeImpact.infectionsByRequestedTime * 0.15;
+  const bedsOccupied = totalHospitalBeds * 0.65;
+  const totalBeds = totalHospitalBeds * 0.95;
+  const availableBeds = totalBeds - bedsOccupied;
+
+  impact.hospitalBedsByRequestedTime =
+    impact.severeCasesByRequestedTime * availableBeds;
+  severeImpact.hospitalBedsByRequestedTime =
+    severeImpact.severeCasesByRequestedTime * availableBeds;
   return {
     data: input,
     impact: impact,
