aa7aef75a666afb6840366dd0acfdc0e5f2a48b8
jeffngugi <ngugimwathi254@gmail.com>
2020-04-14 01:02:05 +0300

    firist challeng completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,29 @@
 const covid19ImpactEstimator = (data) => {
+  const {
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  } = data;
+
+  const impact = {};
+  const severeImpact = {};
   const input = data;
-  const currentlyInfected = data.reportedCases * 10;
+
+  //challenge one
+  severeImpact.currentlyInfected = reportedCases * 50;
+  impact.currentlyInfected = reportedCases * 10;
+  infectionsByRequestedTime = currentlyInfected * 1024;
+
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
+
   return {
     data: input,
-    impact: {},
-    severeImpact: {}
+    impact: impact,
+    severeImpact: severeImpact
   };
 };
 
-export default covid19ImpactEstimator;
+exports.covid19ImpactEstimator = covid19ImpactEstimator;
